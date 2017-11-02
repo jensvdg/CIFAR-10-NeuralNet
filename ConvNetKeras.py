f6286d393d9b6c78fa16559f436036acecafb262
Jens van de Graaf <jens.van.de.graaf@dpdk.com>
2017-11-02 20:05:28 +0100

    Added callbacks to stop training at a more optimal time and to save the settings

--- ConvNetKeras.py
+++ ConvNetKeras.py
@@ -1,7 +1,8 @@
 from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import numpy as np
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data() 
@@ -19,11 +20,17 @@ Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10) 
 
 # Define the ConvNet Model
+callbacks = [
+    EarlyStopping(monitor='val_loss', patience=2, verbose=0),
+    ModelCheckpoint('./checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', save_best_only=True, verbose=0),
+]
+
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(height, width, depth)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(height, width, depth)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
-model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
@@ -34,8 +41,9 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(X_train, Y_train,
-          batch_size=32, epochs=100,
-          verbose=1, validation_split=0.1)
+          batch_size=1000, epochs=120,
+          verbose=1, validation_split=0.1,
+          callbacks=callbacks)
 
 score = model.evaluate(X_test, Y_test, verbose=0)
 print score
